c0dba45e3caaeec87ad9a64ccf37822e92994687
aminul9755 <aminul9755@gmail.com>
2024-11-02 22:13:05 +0100

    Create input section

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container')
+
+function addTask () {
+  if (inputBox.value === '') {
+    alert('You must type something in the box!')
+  } else {
+    let li = document.createElement('li');
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement('span');
+    span.innerHTML = 'Delete';
+    li.appendChild(span);
+  }
+  inputBox.value = '';
+
+  saveData();
+}
+
+listContainer.addEventListener('click', function(e){
+  if (e.target.tagName ==='LI') {
+    e.target.classList.toggle('marked');
+    saveData();
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
+
+function saveData () {
+  localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask () {
+  listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
